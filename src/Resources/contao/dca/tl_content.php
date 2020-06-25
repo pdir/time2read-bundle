7dc9144cfcb53a15955ab14455c8bca47ee9f879
Mathias <develop@pdir.de>
2020-06-25 21:25:30 +0200

    add reading time field

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -7,9 +7,18 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['ttr_activate_reading_time'] = [
     'sql' => "char(1) NOT NULL default ''"
 ];
 
+$GLOBALS['TL_DCA']['tl_content']['fields']['ttr_reading_time'] = [
+    'label' => &$GLOBALS['TL_LANG']['tl_user']['ttr_reading_time'],
+    'inputType' => 'text',
+    'eval' => ['tl_class'=>'w50'],
+    'sql' => "int(1) NOT NULL default ''"
+];
+
+
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
 PaletteManipulator::create()
     ->addField('ttr_activate_reading_time', 'text')
+    ->addField('ttr_reading_time', 'ttr_activate_reading_time')
     ->applyToPalette('text', 'tl_content')
 ;
