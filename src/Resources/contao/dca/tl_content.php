ee4ab74ad8d9f4185f12cf7e0c0aa71715d62ce2
Heiko Peschke <heiko.peschke@oroe.de>
2020-06-26 14:08:08 +0200

    DCA Checkbox Feld für Aktivierung der Lesezeit angelegt

---
+++ src/Resources/contao/dca/tl_content.php
@@ -0,0 +1,16 @@
+<?php
+
+use Contao\CoreBundle\DataContainer\PaletteManipulator;
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['ttr_activate_reading_time'] => [
+    'label'                 => &$GLOBALS['TL_LANG']['tl_content']['ttr_activate_reading_time'],
+    'exclude'               => true,
+    'inputType'             => 'checkbox',
+    'default'               => 0,
+    'eval'                  => ['tl_class'=>'w50'],
+    'sql'                   => "char(1) NOT NULL default ''"
+];
+
+PaletteManipulator::create()
+    ->addField('ttr_activate_reading_time', 'title_legend', PaletteManipulator::POSITION_APPEND)
+    ->applyToPalette('text', 'tl_content');
