17807b89ecac8cc6af919e9fc07374c3034f24b9
Heiko Peschke <heiko.peschke@oroe.de>
2020-06-26 14:50:35 +0200

    Textfeld für Lesezeit angelegt

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -11,6 +11,15 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['ttr_activate_reading_time'] = [
     'sql'                   => "char(1) NOT NULL default ''"
 ];
 
+$GLOBALS['TL_DCA']['tl_content']['fields']['ttr_reading_time'] = [
+    'label'                 => &$GLOBALS['TL_LANG']['tl_content']['ttr_reading_time'],
+    'exclude'               => true,
+    'inputType'             => 'text',
+    'eval'                  => ['maxlength' => 3, 'rgxp' => 'digit'],
+    'sql'                   => "int(3) unsigned NOT NULL default '0'"
+];
+
 PaletteManipulator::create()
     ->addField('ttr_activate_reading_time', 'text', PaletteManipulator::POSITION_AFTER)
+    ->addField('ttr_reading_time', 'ttr_activate_reading_time', PaletteManipulator::POSITION_AFTER)
     ->applyToPalette('text', 'tl_content');
